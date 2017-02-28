2d5f33598150c90e4442ea79c2633c35084b88a6
Pastel-Prince <voo0001@jmss.vic.edu.au>
2017-02-28 17:02:19 +1100

    made it gud

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var xspacing = 10;    // Distance between each horizontal location
+var xspacing = 5;    // Distance between each horizontal location
 var w;                // Width of entire wave
 var theta = 0.0;  // Start angle at 0
 var period = 200.0;   // How many pixels before the wave repeats
@@ -20,6 +20,7 @@ function draw() {
   background(0);
   calcWave();
   renderWave();
+  initialBall();
   finalBall();
     var period = perslide.value()
     dx = (TWO_PI / period) * xspacing;
@@ -42,20 +43,30 @@ function calcWave() {
 
 function renderWave() {
   noStroke();
-  fill(255)
+  var period = perslide.value()
+  var color= map(period,100,300,187,0)
+  fill(color,100,50)
   // A simple way to draw the wave with an ellipse at each location
   for (var x = 0; x < yvalues.length; x++) {
     ellipse(1200-x*xspacing, height/2+yvalues[x], 16, 16);
   }
 
 }
+function initialBall() {
+    var period = perslide.value();
+    var amplitude = ampslide.value();
+    var wig = map(period,100,300,10,0) + map(amplitude,20,100,0,10)
+    wiggle = random(0,wig)
+    size = map(period,100,300,100,0) + map(amplitude,20,100,0,100)
+    color = color = map(period,100,300,25,0) + map(amplitude,20,100,0,25)
+    fill(360,100,color+25)
+    ellipse(0+wiggle,height/2,size+30,size+30)
+}
+
 function finalBall() {
   var period= perslide.value()
-  color = map(period,100,300,70,10) + map(amplitude,20,100,10,70)
+  color = map(period,100,300,65,10) + map(amplitude,20,100,10,65)
   fill(360 ,100,color)
-  for (var x = 0; x < yvalues.length; x++) {
-    ellipse(width-25,height/2+yvalues[2],50,50)
-  }
 
   wiggle = map(period,100,300,70,10) + map(amplitude,20,100,10,70)
 
